28e8ced825268cc1deb538f83efe08f4e5b5210b
Honeywell <ksy95703@naver.com>
2018-07-13 17:46:08 +0900

    without enter

--- calc/Form1.Designer.cs
+++ calc/Form1.Designer.cs
@@ -46,6 +46,7 @@
             this.button15 = new System.Windows.Forms.Button();
             this.button16 = new System.Windows.Forms.Button();
             this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -66,7 +67,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button_num_Click);
+            this.button1.Click += new System.EventHandler(this.button_Click);
             // 
             // button2
             // 
@@ -76,7 +77,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button_num_Click);
+            this.button2.Click += new System.EventHandler(this.button_Click);
             // 
             // button3
             // 
@@ -86,7 +87,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button_num_Click);
+            this.button3.Click += new System.EventHandler(this.button_Click);
             // 
             // button4
             // 
@@ -96,7 +97,7 @@
             this.button4.TabIndex = 4;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button_num_Click);
+            this.button4.Click += new System.EventHandler(this.button_Click);
             // 
             // button5
             // 
@@ -106,7 +107,7 @@
             this.button5.TabIndex = 5;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button_num_Click);
+            this.button5.Click += new System.EventHandler(this.button_Click);
             // 
             // button6
             // 
@@ -116,7 +117,7 @@
             this.button6.TabIndex = 6;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button_num_Click);
+            this.button6.Click += new System.EventHandler(this.button_Click);
             // 
             // button7
             // 
@@ -126,7 +127,7 @@
             this.button7.TabIndex = 7;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button_num_Click);
+            this.button7.Click += new System.EventHandler(this.button_Click);
             // 
             // button8
             // 
@@ -136,7 +137,7 @@
             this.button8.TabIndex = 8;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button_num_Click);
+            this.button8.Click += new System.EventHandler(this.button_Click);
             // 
             // button9
             // 
@@ -146,17 +147,17 @@
             this.button9.TabIndex = 9;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button_num_Click);
+            this.button9.Click += new System.EventHandler(this.button_Click);
             // 
             // button10
             // 
             this.button10.Location = new System.Drawing.Point(28, 253);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(112, 46);
+            this.button10.Size = new System.Drawing.Size(53, 46);
             this.button10.TabIndex = 10;
             this.button10.Text = "0";
             this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button_num_Click);
+            this.button10.Click += new System.EventHandler(this.button_Click);
             // 
             // button11
             // 
@@ -164,9 +165,9 @@
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(53, 46);
             this.button11.TabIndex = 11;
-            this.button11.Text = ".";
+            this.button11.Text = ")";
             this.button11.UseVisualStyleBackColor = true;
-            this.button11.Click += new System.EventHandler(this.button11_Click);
+            this.button11.Click += new System.EventHandler(this.button_close_Click);
             // 
             // button12
             // 
@@ -176,7 +177,7 @@
             this.button12.TabIndex = 12;
             this.button12.Text = "+";
             this.button12.UseVisualStyleBackColor = true;
-            this.button12.Click += new System.EventHandler(this.button12_Click);
+            this.button12.Click += new System.EventHandler(this.button_Click);
             // 
             // button13
             // 
@@ -186,7 +187,7 @@
             this.button13.TabIndex = 13;
             this.button13.Text = "-";
             this.button13.UseVisualStyleBackColor = true;
-            this.button13.Click += new System.EventHandler(this.button13_Click);
+            this.button13.Click += new System.EventHandler(this.button_Click);
             // 
             // button14
             // 
@@ -196,7 +197,7 @@
             this.button14.TabIndex = 14;
             this.button14.Text = "*";
             this.button14.UseVisualStyleBackColor = true;
-            this.button14.Click += new System.EventHandler(this.button14_Click);
+            this.button14.Click += new System.EventHandler(this.button_Click);
             // 
             // button15
             // 
@@ -206,7 +207,7 @@
             this.button15.TabIndex = 15;
             this.button15.Text = "/";
             this.button15.UseVisualStyleBackColor = true;
-            this.button15.Click += new System.EventHandler(this.button15_Click);
+            this.button15.Click += new System.EventHandler(this.button_Click);
             // 
             // button16
             // 
@@ -216,6 +217,7 @@
             this.button16.TabIndex = 16;
             this.button16.Text = "=";
             this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button_e_Click);
             // 
             // button17
             // 
@@ -225,13 +227,24 @@
             this.button17.TabIndex = 17;
             this.button17.Text = "C";
             this.button17.UseVisualStyleBackColor = true;
-            this.button17.Click += new System.EventHandler(this.button17_Click);
+            this.button17.Click += new System.EventHandler(this.button_C_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(87, 253);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(53, 46);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "(";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button_open_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(375, 319);
+            this.Controls.Add(this.button18);
             this.Controls.Add(this.button17);
             this.Controls.Add(this.button16);
             this.Controls.Add(this.button15);
@@ -277,6 +290,7 @@
         private System.Windows.Forms.Button button15;
         private System.Windows.Forms.Button button16;
         private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
     }
 }
 
--- calc/Form1.cs
+++ calc/Form1.cs
@@ -16,45 +16,135 @@ namespace calc
         {
             InitializeComponent();
         }
-
-        private void button_num_Click(object sender, EventArgs e)
+        
+        private void button_Click(object sender, EventArgs e)
         {
             Button _button;
             _button = (Button)sender;
             textBox1.Text += _button.Text;
+            
         }
-
-        private void button11_Click(object sender, EventArgs e)
+        private void newText_KeyDown(object sender, KeyEventArgs e)
         {
-            if (!this.textBox1.Text.Contains("."))
+            if (e.KeyCode == Keys.Enter)
             {
-                textBox1.Text += ".";
+                var cal = textBox1.Text.ToCharArray();
+                string s = calculate(cal);
+                textBox1.Text = s;
             }
+                
         }
 
-        private void button12_Click(object sender, EventArgs e)
+        private void button_close_Click(object sender, EventArgs e)
         {
-            textBox1.Text += "+";
+            textBox1.Text += button11.Text;    
         }
-
-        private void button13_Click(object sender, EventArgs e)
+        private void button_open_Click(object sender, EventArgs e)
         {
-            textBox1.Text += "-";
+            textBox1.Text += button18.Text;
         }
-
-        private void button14_Click(object sender, EventArgs e)
-        {
-            textBox1.Text += "*";
-        }
-
-        private void button15_Click(object sender, EventArgs e)
-        {
-            textBox1.Text += "/";
-        }
-
-        private void button17_Click(object sender, EventArgs e)
+        private void button_C_Click(object sender, EventArgs e)
         {
             textBox1.Text = string.Empty;
         }
+
+        private void button_e_Click(object sender, EventArgs e)
+        {
+            var cal=textBox1.Text.ToCharArray();
+            string s = calculate(cal);
+            string ans = make_answer(s);
+            textBox1.Text = ans;
+        }
+        private string make_answer(string s)
+        {
+            var cal = s.ToCharArray();
+            string[] stack = new string[30];
+            int cnt = 0;
+            for(int i = 0; i < cal.Length; i++)
+            {
+                if (cal[i] == '+')
+                {
+                    int a = Convert.ToInt32(stack[cnt - 2]) + Convert.ToInt32(stack[cnt - 1]);
+                    stack[cnt - 2] = a.ToString();
+                    cnt--;
+                }else if (cal[i] == '-')
+                {
+                    int a = Convert.ToInt32(stack[cnt - 2]) - Convert.ToInt32(stack[cnt - 1]);
+                    stack[cnt - 2] = a.ToString();
+                    cnt--;
+                }
+                else if (cal[i] == '*')
+                {
+                    int a = Convert.ToInt32(stack[cnt - 2]) * Convert.ToInt32(stack[cnt - 1]);
+                    stack[cnt - 2] = a.ToString();
+                    cnt--;
+                }
+                else if (cal[i] == '/')
+                {
+                    float a = Convert.ToInt32(stack[cnt - 2]) / Convert.ToInt32(stack[cnt - 1]);
+                    stack[cnt - 2] = a.ToString();
+                    cnt--;
+                }
+                else
+                {
+                    stack[cnt] = cal[i].ToString();
+                    cnt++;
+                }
+            }return stack[0];
+        }
+
+        private int get_prt(char c)
+        {
+            if (c == '*' || c == '/')
+            {
+                return 9;
+            }
+            else
+            {
+                return 5;
+            }
+        }
+        private string calculate(char[] cal)
+        {
+            string exp = "";
+            char[] stack = new char[30];
+            int cnt = 0;
+            for (int i = 0; i < cal.Length; i++)
+            {
+                if(cal[i]=='(')
+                {
+                    string m = "";
+                    i++;
+                    while (cal[i] != ')')
+                    {
+                        m += cal[i];
+                        i++;
+                    }
+                    exp += calculate(m.ToCharArray());
+                }
+
+                else if(cal[i]=='+'||cal[i]=='-'||cal[i]=='*'||cal[i]=='/'){
+                    int prt = get_prt(cal[i]);
+                    if (cnt != 0)
+                    {
+                        int postprt = get_prt(stack[cnt - 1]);
+                        while (prt <= postprt&&cnt>0)
+                        {
+                            exp += stack[cnt - 1];
+                            cnt--;
+                        }
+                    }
+                    stack[cnt] = cal[i];
+                    cnt++;
+                }
+                else
+                {
+                    exp += cal[i];
+                }
+            }exp += stack[0];
+            return exp;
+        }
+
+        
     }
 }
